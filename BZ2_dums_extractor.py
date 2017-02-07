50887565d403421d4177078499cd7e92c0285581
Abolfazl Mahdizade <abolfazl.mahdizade@gmail.com>
2017-02-07 18:23:32 +0330

    infoboxes, abstracts, revision_ids and wiki_text extraction complete

--- BZ2_dums_extractor.py
+++ BZ2_dums_extractor.py
@@ -1,7 +1,7 @@
 import gc
-import json
 import logging
 import os
+import re
 from os.path import join
 
 import wikitextparser as wtp
@@ -44,8 +44,17 @@ def extract_wikipedia_bz2_dump(input_filename, output_dir):
 
 def extract_infoboxes_abstracts(filename):
     pages_path_dict = dict()
-    infoboxes_dict = dict()
-    abstracts_dict = dict()
+    with_infoboxes_dict = dict()
+    without_infoboxes_dict = dict()
+
+    infoboxes_flag = Config.information_flags['infoboxes_flag']
+    abstracts_flag = Config.information_flags['abstracts_flag']
+    revision_id_flag = Config.information_flags['revision_id_flag']
+    wiki_text_flag = Config.information_flags['wiki_text_flag']
+
+    without_infoboxes_dict[abstracts_flag] = dict()
+    without_infoboxes_dict[revision_id_flag] = dict()
+    without_infoboxes_dict[wiki_text_flag] = dict()
 
     pages_counter = 0
     input_filename = join(Config.extracted_pages_articles_dir, filename)
@@ -62,8 +71,8 @@ def extract_infoboxes_abstracts(filename):
 
         text = parsed_page.revision.find('text').text
         page_name = parsed_page.title.text
-        if page_name not in pages_path_dict:
-            pages_path_dict[page_name] = list()
+        revision_id = parsed_page.revision.id.text
+        pages_path_dict[page_name] = list()
         wiki_text = wtp.parse(text)
 
         has_infobox = False
@@ -72,38 +81,33 @@ def extract_infoboxes_abstracts(filename):
             infobox_name, infobox_type = Utils.find_get_infobox_name_type(template.name)
             if infobox_name and infobox_type:
                 has_infobox = True
-                if page_name not in infoboxes_dict:
-                    infoboxes_dict[page_name] = dict()
-                if infobox_name not in infoboxes_dict[page_name]:
-                    infoboxes_dict[page_name][infobox_name] = dict()
-                if infobox_type not in infoboxes_dict[page_name][infobox_name]:
-                    infoboxes_dict[page_name][infobox_name][infobox_type] = dict()
+                if infobox_name not in with_infoboxes_dict:
+                    with_infoboxes_dict[infobox_name] = dict()
 
+                if infobox_type not in with_infoboxes_dict[infobox_name]:
+                    with_infoboxes_dict[infobox_name][infobox_type] = dict()
+
+                for flag in Config.information_flags:
+                    information_flag = Config.information_flags[flag]
+                    if information_flag not in with_infoboxes_dict[infobox_name][infobox_type]:
+                        with_infoboxes_dict[infobox_name][infobox_type][information_flag] = dict()
+
+                if page_name not in with_infoboxes_dict[infobox_name][infobox_type][infoboxes_flag]:
+                    with_infoboxes_dict[infobox_name][infobox_type][infoboxes_flag][page_name] = list()
+
+                infobox_dict = dict()
                 for param in template.arguments:
                     param_name = clean(str(param.name))
                     param_value = clean(str(param.value))
                     if param_value:
-                        infoboxes_dict[page_name][infobox_name][infobox_type][param_name] = param_value
+                        param_value = re.split(r'\\\\|/|,|،', param_value)
+                        param_value = [value.strip() for value in param_value]
+                        infobox_dict[param_name] = param_value
 
-                relative_resource_path = join(infobox_name, infobox_type, page_name.replace('/', '∕'))
-                if len(relative_resource_path) < 255:
-                    absolute_resource_path = join(Config.extracted_pages_with_infobox_dir,
-                                                  relative_resource_path)
-                    pages_path_dict[page_name].append(absolute_resource_path)
+                with_infoboxes_dict[infobox_name][infobox_type][infoboxes_flag][page_name].append(infobox_dict)
 
-                    Utils.create_directory(absolute_resource_path)
-                    infobox_filename = Utils.get_information_filename(absolute_resource_path, 'infobox')
-                    infobox_file = open(infobox_filename, 'w+', encoding='utf8')
-                    infobox_json = json.dumps(infoboxes_dict[page_name][infobox_name][infobox_type],
-                                              ensure_ascii=False, indent=2, sort_keys=True)
-                    infobox_file.write(infobox_json)
-                    infobox_file.close()
-
-        relative_resource_path = join(page_name[0].replace('/', '∕'), page_name[:2].replace('/', '∕'),
-                                      page_name.replace('/', '∕'))
-        if not has_infobox and len(relative_resource_path) < 255:
-            absolute_resource_path = join(Config.extracted_pages_without_infobox_dir, relative_resource_path)
-            pages_path_dict[page_name].append(absolute_resource_path)
+                if len(with_infoboxes_dict[infobox_name][infobox_type][infoboxes_flag][page_name]) == 1:
+                    pages_path_dict[page_name].append([infobox_name, infobox_type])
 
         first_section = wiki_text.sections[0]
         abstract = first_section.string
@@ -118,25 +122,41 @@ def extract_infoboxes_abstracts(filename):
                     abstract = abstract.replace(template.string, '')
 
             abstract = clean(abstract, specify_wikilinks=False).replace('()', '')
-            abstracts_dict[page_name] = abstract
-
-            for path in pages_path_dict[page_name]:
-                absolute_resource_path = path
-                if page_name in abstracts_dict:
-                    Utils.create_directory(absolute_resource_path)
-                    abstract_filename = join(absolute_resource_path, 'abstract.txt')
-                    abstract_file = open(abstract_filename, 'w+', encoding='utf8')
-                    abstract_file.write(abstracts_dict[page_name])
-                    abstract_file.close()
+            if has_infobox:
+                for path in pages_path_dict[page_name]:
+                    infobox_name = path[0]
+                    infobox_type = path[1]
+                    with_infoboxes_dict[infobox_name][infobox_type][abstracts_flag][page_name] = abstract
+                    with_infoboxes_dict[infobox_name][infobox_type][revision_id_flag][page_name] = revision_id
+                    with_infoboxes_dict[infobox_name][infobox_type][wiki_text_flag][page_name] = text
+            else:
+                without_infoboxes_dict[abstracts_flag][page_name] = abstract
+                without_infoboxes_dict[revision_id_flag][page_name] = revision_id
+                without_infoboxes_dict[wiki_text_flag][page_name] = text
 
         del templates
         del wiki_text
 
-    logging_information_extraction(pages_counter, input_filename)
+    for infobox_name in with_infoboxes_dict:
+        for infobox_type in with_infoboxes_dict[infobox_name]:
+            absolute_resource_path = join(Config.extracted_pages_with_infobox_dir, infobox_name, infobox_type)
+            if len(absolute_resource_path) < 255:
+                Utils.create_directory(absolute_resource_path)
 
-    Utils.create_directory(Config.extracted_pages_path_dir, show_logging=True)
-    pages_path_filename = Utils.get_information_filename(Config.extracted_pages_path_dir, filename)
-    pages_path_file = open(pages_path_filename, 'w+', encoding='utf8')
-    pages_path_json = json.dumps(pages_path_dict, ensure_ascii=False, indent=2, sort_keys=True)
-    pages_path_file.write(pages_path_json)
-    pages_path_file.close()
+                for flag in Config.information_flags:
+                    information_flag = Config.information_flags[flag]
+                    information_filename = Utils.get_information_filename(absolute_resource_path,
+                                                                          filename+'-'+information_flag)
+                    Utils.save_dict_to_json_file(information_filename,
+                                                 with_infoboxes_dict[infobox_name][infobox_type][information_flag])
+
+    absolute_resource_path = Config.extracted_pages_without_infobox_dir
+    if len(absolute_resource_path) < 255:
+        Utils.create_directory(absolute_resource_path)
+        for flag in Config.information_flags:
+            information_flag = Config.information_flags[flag]
+            if information_flag != infoboxes_flag:
+                filename = Utils.get_information_filename(absolute_resource_path, filename + '-' + information_flag)
+                Utils.save_dict_to_json_file(filename, without_infoboxes_dict[information_flag])
+
+    logging_information_extraction(pages_counter, input_filename)
