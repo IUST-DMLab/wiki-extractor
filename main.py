bc1bf0a9934a5dbcdabb2bdf0a24413a3eb9606d
Abolfazl Mahdizade <abolfazl.mahdizade@gmail.com>
2017-02-19 13:31:32 +0330

    add enwiki extraction

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import BZ2_dums_extractor
 import Wiki_sql_extractor
 
+import Config
+
 
 def main():
     # BZ2_dums_extractor.extract_bz2_dump('25')
     # BZ2_dums_extractor.extract_all()
-    Wiki_sql_extractor.extract_all()
+    # Wiki_sql_extractor.extract_all()
+
+    BZ2_dums_extractor.extract_wikipedia_bz2_dump(Config.enwiki_latest_pages_articles_dump,
+                                                  Config.en_extracted_pages_articles_dir)
 
 
 if __name__ == '__main__':
